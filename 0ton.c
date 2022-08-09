9115924b5574c4d50a04e407a2726f1bef917ba6
imdevsu <110780393+imdevsu@users.noreply.github.com>
2022-08-09 10:04:59 +0530

    Add files via upload

---
+++ 0ton.c
@@ -0,0 +1,17 @@
+
+
+//print the number from 0 to n & if n is given by user 
+#include<stdio.h>
+ 
+ int main()
+ { 
+     int n;
+     printf("Enter Any Nymber:");
+     scanf("%d",&n);
+     
+     for(int i=0; i<=n; i++) {
+      printf("%d\n",i); }
+      
+      return 0;
+     
+}
